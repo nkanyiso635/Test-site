627210e6a9080d6b86880855e874ac381cc1a121
nkanyiso635 <nkanyiso.enquiries@gmail.com>
2024-11-13 21:47:43 +0200

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://test-site-backend-gj2b.onrender.com"
+    const url = "https://test-site-backend-la6r.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
